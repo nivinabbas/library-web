71e7bbda6f33cd1eed4a6ef76c880ea6a34aa715
Your Name <marahnairat0@gmail.com>
2019-06-07 01:31:39 +0300

    modifying functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 session_start();
 
 // connect to database
-$db = mysqli_connect('localhost', 'root', '', 'library-web');
+$db = mysqli_connect('localhost', 'root', '', '');
 
 // variable declaration
 $username = "";
