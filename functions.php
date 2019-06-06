796e6b2afdda5050bc970a49195d96c518d5e5e7
Your Name <marahnairat0@gmail.com>
2019-06-07 01:08:40 +0300

    modifying functions.php

--- functions.php
+++ functions.php
@@ -92,4 +92,71 @@ function display_error() {
 			}
 		echo '</div>';
 	}
+	function isLoggedIn()
+	{
+		if (isset($_SESSION['user'])) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+	if (isset($_GET['logout'])) {
+		session_destroy();
+		unset($_SESSION['user']);
+		header("location: login.php");
+	}
+	if (isset($_POST['login_btn'])) {
+		login();
+	}
+	
+	// LOGIN USER
+	function login(){
+		global $db, $username, $errors;
+	
+		// grap form values
+		$username = e($_POST['username']);
+		$password = e($_POST['password']);
+	
+		// make sure form is filled properly
+		if (empty($username)) {
+			array_push($errors, "Username is required");
+		}
+		if (empty($password)) {
+			array_push($errors, "Password is required");
+		}
+	
+		// attempt login if no errors on form
+		if (count($errors) == 0) {
+			$password = md5($password);
+	
+			$query = "SELECT * FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+			$results = mysqli_query($db, $query);
+	
+			if (mysqli_num_rows($results) == 1) { // user found
+				// check if user is admin or user
+				$logged_in_user = mysqli_fetch_assoc($results);
+				if ($logged_in_user['user_type'] == 'admin') {
+	
+					$_SESSION['user'] = $logged_in_user;
+					$_SESSION['success']  = "You are now logged in";
+					header('location: admin/home.php');		  
+				}else{
+					$_SESSION['user'] = $logged_in_user;
+					$_SESSION['success']  = "You are now logged in";
+	
+					header('location: index.php');
+				}
+			}else {
+				array_push($errors, "Wrong username/password combination");
+			}
+		}
+	}
+	function isAdmin()
+	{
+		if (isset($_SESSION['user']) && $_SESSION['user']['user_type'] == 'admin' ) {
+			return true;
+		}else{
+			return false;
+		}
+	}
 }	
