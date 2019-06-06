216154ba9fdd3f4e7efde33e5837529645c41c03
nivinabbas <nivin.abbas1@gmail.com>
2019-06-07 02:00:22 +0300

    fix errors in function.php,index.php and connecting to database E-library

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+
 <?php 
     include('functions.php');
     if (!isLoggedIn()) {
         $_SESSION['msg'] = "You must log in first";
-        header('location: login.php');
+        header('location: login.php');}
 ?>
 <!DOCTYPE html>
 <html>
