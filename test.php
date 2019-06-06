c9998ed47f0dd22c92e5d1b9ebb7ee34b0188d7b
modeh <modeh@vernegroup.com>
2019-06-06 18:46:56 +0300

    fixing semicolon

--- test.php
+++ test.php
@@ -1,8 +1,5 @@
 <?php echo "this is a test file from maysara to collaborate in github" ;
 echo "this is a chnage from nivin ";
-<<<<<<< HEAD
-echo "change from maysara"
-=======
+echo "change from maysara";
 echo "hi";
->>>>>>> 71965b9e84592fed20d6f6759854cc4b7998ebdd
 ?>
