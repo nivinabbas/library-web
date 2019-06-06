fd4a96dee2e24aba7fcecf413f80b0cb06258c3c
nivinabbas <nivin.abbas1@gmail.com>
2019-06-06 20:33:54 +0300

    add commands in index.php

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
-<?php echo "this is a test file from maysara to collaborate in github" ;
+<?php 
+echo "this is a test file from maysara to collaborate in github" ;
 echo "this is a chnage from nivin ";
 echo "change from maysara";
 echo "hi";
